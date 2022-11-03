7a9eddcce8e80e3a0bdcd7c8804d8f1333902419
alinaliuu <alina.yixuan.liu@gmail.com>
2022-11-03 18:39:51 -0400

    //

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/autonomous1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/autonomous1.java
@@ -8,17 +8,79 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
 import Cryptic.OurRobot;
 
 @TeleOp(name="allenTest")
 public class autonomous1 extends LinearOpMode {
     OurRobot robot = new OurRobot();
+
+    private static final String TFOD_MODEL_ASSET = "PowerPlay.tflite";
+    // private static final String TFOD_MODEL_FILE  = "/sdcard/FIRST/tflitemodels/CustomTeamModel.tflite";
+
+
+    private static final String[] LABELS = {
+            "1 Bolt",
+            "2 Bulb",
+            "3 Panel"
+    };
+
+    /*
+     * IMPORTANT: You need to obtain your own license key to use Vuforia. The string below with which
+     * 'parameters.vuforiaLicenseKey' is initialized is for illustration only, and will not function.
+     * A Vuforia 'Development' license key, can be obtained free of charge from the Vuforia developer
+     * web site at https://developer.vuforia.com/license-manager.
+     *
+     * Vuforia license keys are always 380 characters long, and look as if they contain mostly
+     * random data. As an example, here is a example of a fragment of a valid key:
+     *      ... yIgIzTqZ4mWjk9wd3cZO9T1axEqzuhxoGlfOOI2dRzKS4T0hQ8kT ...
+     * Once you've obtained a license key, copy the string from the Vuforia web site
+     * and paste it in to your code on the next line, between the double quotes.
+     */
+    private static final String VUFORIA_KEY = "ARUPuDz/////AAABmRyLfAbzmE22hUVJ/Mw7oxU4prx4LUxeMwyyDVqQJlHAP1OCXDxxKQkpXnJJMMMq+oK3dzlR2rs+u6kwTi0RHPbcpcLSVjYBN6Y4gcrEuRYL2p2Mw5d+SXooW/rj+5sptCJQ8TZ0+I5E0fOk1ra5eG1lAi53cCcMrHlGOU+nWOwSQqWlUGHADWXwBIlhggVCaoO9dIyfUw1CJE0L6JBechr2Zpe5Dkhj84uAqU8yNEn+NFyiARdr9j0RjK0ZRYjAvEXyUHCRAqAeUnE3f+Hgsrc9Y4h8JvcsI/uxW4hpLtaXmqLMqF74y9yvCudLhq0twYQEj8U4tjLllFa6twz2C4sEipveznEkyYLvoSNe1LDU";
+    private VuforiaLocalizer vuforia;
+
+    private TFObjectDetector tfod;
+
     @Override
     public void runOpMode() throws InterruptedException {
         OurRobot.initialize(this);
 
+        // The TFObjectDetector uses the camera frames from the VuforiaLocalizer, so we create that
+        // first.
+        initVuforia();
+        initTfod();
+
+        /**
+         * Activate TensorFlow Object Detection before we wait for the start command.
+         * Do it here so that the Camera Stream window will have the TensorFlow annotations visible.
+         **/
+        if (tfod != null) {
+            tfod.activate();
+
+            // The TensorFlow software will scale the input images from the camera to a lower resolution.
+            // This can result in lower detection accuracy at longer distances (> 55cm or 22").
+            // If your target is at distance greater than 50 cm (20") you can increase the magnification value
+            // to artificially zoom in to the center of image.  For best results, the "aspectRatio" argument
+            // should be set to the value of the images used to create the TensorFlow Object Detection model
+            // (typically 16/9).
+            tfod.setZoom(1.0, 16.0/9.0);
+        }
+
+        /** Wait for the game to begin */
+        telemetry.addData(">", "Press Play to start op mode");
+        telemetry.update();
+
         waitForStart();
 
+        //actuql auto code
+        objectDetection();
+/*
         ElapsedTime timer = new ElapsedTime();
 
         telemetry.addData("Status", "Running");
@@ -29,6 +91,64 @@ public class autonomous1 extends LinearOpMode {
             robot.dt.leftFront.setPower(0.7);
             robot.dt.rightBack.setPower(0.7);
             robot.dt.leftBack.setPower(0.7);
+        }
+        */
+    }
+    private void initVuforia() {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         */
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+    }
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minResultConfidence = 0.75f;
+        tfodParameters.isModelTensorFlow2 = true;
+        tfodParameters.inputSize = 300;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+
+        // Use loadModelFromAsset() if the TF Model is built in as an asset by Android Studio
+        // Use loadModelFromFile() if you have downloaded a custom team model to the Robot Controller's FLASH.
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABELS);
+        // tfod.loadModelFromFile(TFOD_MODEL_FILE, LABELS);
+    }
+    private void objectDetection () {
+        if (opModeIsActive()) {
+            int count = 0;
+            while (opModeIsActive() && count <= 10) {
+                if (tfod != null) {
+                    // getUpdatedRecognitions() will return null if no new information is available since
+                    // the last time that call was made.
+                    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                    if (updatedRecognitions != null) {
+                        telemetry.addData("# Objects Detected", updatedRecognitions.size());
+
+                        // step through the list of recognitions and display image position/size information for each one
+                        // Note: "Image number" refers to the randomized image orientation/number
+                        for (Recognition recognition : updatedRecognitions) {
+                            double col = (recognition.getLeft() + recognition.getRight()) / 2;
+                            double row = (recognition.getTop() + recognition.getBottom()) / 2;
+                            double width = Math.abs(recognition.getRight() - recognition.getLeft());
+                            double height = Math.abs(recognition.getTop() - recognition.getBottom());
+
+                            telemetry.addData("", " ");
+                            telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
+                            telemetry.addData("- Position (Row/Col)", "%.0f / %.0f", row, col);
+                            telemetry.addData("- Size (Width/Height)", "%.0f / %.0f", width, height);
+                        }
+                        count++;
+                        telemetry.update();
+                    }
+                }
+            }
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestClass.java
+++
@@ -1,5 +0,0 @@
-package org.firstinspires.ftc.teamcode;
-
-public class TestClass {
- String test = "sussy baka";
-}
